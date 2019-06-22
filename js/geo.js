a2caa127b05001116b49f7ff6320e7d88fe5a7b2
Egor <egoooor100@gmail.com>
2019-06-22 14:46:05 +0300

    1

--- js/geo.js
+++ js/geo.js
@@ -1,21 +1,35 @@
 function b1() {
     document.getElementById('city').firstChild.data = 'Владивосток';
+    document.getElementById('info').firstChild.data = 'Владивосток - Москва: 10 суток';
+    document.getElementById('period').firstChild.data = 'Периодичность отправок: раз в 10 дней';
 }
 function b2() {
     document.getElementById('city').firstChild.data = 'Хабаровск';
+    document.getElementById('info').firstChild.data = '-';
+    document.getElementById('period').firstChild.data = '-';
 }
 function b3() {
     document.getElementById('city').firstChild.data = 'Новосибирск';
+    document.getElementById('info').firstChild.data = '-';
+    document.getElementById('period').firstChild.data = '-';
 }
 function b4() {
     document.getElementById('city').firstChild.data = 'Москва';
+    document.getElementById('info').firstChild.data = '-';
+    document.getElementById('period').firstChild.data = '-';
 }
 function b5() {
     document.getElementById('city').firstChild.data = 'Астара (Иран)';
+    document.getElementById('info').firstChild.data = 'Астара - Москва: 5 суток';
+    document.getElementById('period').firstChild.data = 'Периодичность отправок: раз в 5 дней';
 }
 function b6() {
     document.getElementById('city').firstChild.data = 'Мерсин (Турция)';
+    document.getElementById('info').firstChild.data = 'Мерсин - Москва: 5 суток';
+    document.getElementById('period').firstChild.data = 'Периодичность отправок: раз в неделю';
 }
 function b7() {
     document.getElementById('city').firstChild.data = 'Ташкент, Джизак, Самарканд (Узбекистан)';
+    document.getElementById('info').firstChild.data = 'Узбекистан - Москва: 5 суток';
+    document.getElementById('period').firstChild.data = 'Периодичность отправок: раз в 5 дней';
 }
