4c738a9b9749ccbdc7e713594f1a3dd3551c4e94
Egor <egoooor100@gmail.com>
2019-06-22 03:38:54 +0300

    1

---
+++ js/geo.js
@@ -0,0 +1,21 @@
+function b1() {
+    document.getElementById('city').firstChild.data = 'Владивосток';
+}
+function b2() {
+    document.getElementById('city').firstChild.data = 'Хабаровск';
+}
+function b3() {
+    document.getElementById('city').firstChild.data = 'Новосибирск';
+}
+function b4() {
+    document.getElementById('city').firstChild.data = 'Москва';
+}
+function b5() {
+    document.getElementById('city').firstChild.data = 'Астара (Иран)';
+}
+function b6() {
+    document.getElementById('city').firstChild.data = 'Мерсин (Турция)';
+}
+function b7() {
+    document.getElementById('city').firstChild.data = 'Ташкент, Джизак, Самарканд (Узбекистан)';
+}
